83ed18a62fa5924d44348f4043584fbf03acb307
starryeye <see2818@naver.com>
2023-06-08 12:12:50 +0900

    [starryeye, 2023.06.08] - Post 좋아요 write 부분 개발
    내용
    - Post 좋아요 write 부분 개발

--- src/main/java/dev/practice/snsmysql/domain/post/service/PostLikeWriteService.java
+++ src/main/java/dev/practice/snsmysql/domain/post/service/PostLikeWriteService.java
@@ -4,13 +4,16 @@ import dev.practice.snsmysql.domain.member.dto.MemberDto;
 import dev.practice.snsmysql.domain.post.dto.PostDto;
 import dev.practice.snsmysql.domain.post.entity.PostLike;
 import dev.practice.snsmysql.domain.post.repository.PostLikeRepository;
+import dev.practice.snsmysql.domain.post.repository.PostRedisDataStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 /**
  * Post like 를 위한 서비스
  *
- * TODO: 한명의 회원은 하나의 게시물에 대해 한번만 좋아요를 누를 수 있는 기능은.. DB PostLike Table 의 memberId 에 Unique 제약조건을 걸어서 처리..?
+ * TODO: 한명의 회원은 하나의 게시물에 대해 한번만 좋아요를 누를 수 있는 기능은..
+ *  -> DB PostLike Table 의 memberId 에 Unique 제약조건을 걸어서 처리..?
+ *  -> Client 에서 처리
  *
  * 현재는 memberId, postId 만 사용하지만,
  * memberDto, postDto 를 모두 받기 때문에 추후 다양한 기능을 추가할 수 있다.
@@ -20,6 +23,7 @@ import org.springframework.stereotype.Service;
 public class PostLikeWriteService {
 
     private final PostLikeRepository postLikeRepository;
+    private final PostRedisDataStore postRedisDataStore;
 
     /**
      * 단순히 PostLike 를 insert 하므로 update 처럼 락 처리를 하지 않아도 된다.
@@ -30,6 +34,10 @@ public class PostLikeWriteService {
                 .postId(postDto.id())
                 .build();
 
-        return postLikeRepository.save(postLike).getId();
+        Long postId = postLikeRepository.save(postLike).getId();
+
+        postRedisDataStore.postLikeIncrement(String.valueOf(postId), 1L);
+
+        return postId;
     }
 }
